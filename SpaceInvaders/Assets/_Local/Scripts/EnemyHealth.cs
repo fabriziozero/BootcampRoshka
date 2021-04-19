5e1c1a5b0238c63d545bdc114d7e602be863f6d6
Fabrizio <fabriziozero13@gmail.com>
2021-04-19 03:35:03 -0400

    Se agregaron modelos de Enemigos Rojo y Lila y un script para controlar la vida de los enemigos

---
+++ SpaceInvaders/Assets/_Local/Scripts/EnemyHealth.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    [SerializeField] private float enemyHealth; // variable Salud
+
+    void Awake()
+    {
+        if (this.tag == "RedEnemy")
+        {
+            enemyHealth = 3f;
+        }
+        else if (this.tag == "PurpleEnemy")
+        {
+            enemyHealth = 1f;
+        }
+    }
+    public void set_Health (float health)
+    {
+        enemyHealth += health; 
+    }
+	
+    // Update is called once per frame
+    void Update()
+    {
+        //Si la vida de la base es menor a 0 la base se destruye
+        if (enemyHealth <= 0)
+        {
+            PlayerScore._playerScore += 10;
+            Destroy(gameObject);
+        }
+    }
+}
