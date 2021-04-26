ece509d2bb05822a27202332f138dcba9b0b7748
Fabrizio <fabriziozero13@gmail.com>
2021-04-26 03:24:00 -0400

    modified TreeController, se agregaron listas para saber la cantidad de troncos en el arbol y preparacion para el movimiento de camara. Ahora los Tree estan en el contenedor Forest

--- Trabajo 1/Assets/Scripts/Game/TreeController.cs
+++ Trabajo 1/Assets/Scripts/Game/TreeController.cs
@@ -6,16 +6,35 @@ using UnityEngine;
 public class TreeController : MonoBehaviour
 {
     private GameObject _Tree;
+    //private GameObject _Trunk;
+    [SerializeField] private List<Transform> listTrunk = new List<Transform>();
     // Start is called before the first frame update
+    private void Awake()
+    {
+        //Se llena la lista con los Trunk dentro del contenedor
+        for (int i = 0; i < gameObject.transform.childCount; i++)
+        {
+            Transform tmpTransform = gameObject.transform.GetChild(i);
+            Transform trunk = tmpTransform.gameObject.GetComponent<Transform>();
+            if (trunk != null)
+            {
+                Debug.Log("TrunkCount= "+ i);
+                listTrunk.Add(trunk);
+            }
+
+        }
+    }
     void Start()
     {
         _Tree = this.gameObject;
-        LeanTween.moveY(_Tree, 3, 1.5f).setEaseOutBack();
+        
         
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void FixedUpdate()
+    {   
+        
+        LeanTween.moveY(_Tree, 2, 5f*Time.fixedDeltaTime).setEaseOutBack();
     }
 }
